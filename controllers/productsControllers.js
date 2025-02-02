4fe62cf6a4e738fa8755315200e56159055ec63a
vatiza <rakibur.mac@gmail.com>
2025-02-02 23:39:35 +0600

    product api add

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,8 +1,26 @@
 const { db } = require("../config/db");
 
 const getProducts = async (req, res) => {
-  const products = await db.collection("products").find().toArray();
-  res.json(products);
+  try {
+    const { category, subcategory } = req.query;
+    let filter = {};
+
+    if (category) {
+      filter["category.label"] = { $regex: new RegExp(category, "i") };
+    }
+
+    if (subcategory) {
+      filter["category.subcategory.label"] = {
+        $regex: new RegExp(subcategory, "i"),
+      };
+    }
+
+    const products = await db.collection("products").find(filter).toArray();
+    res.json(products);
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 const getHighLights = async (req, res) => {
   try {
@@ -21,25 +39,30 @@ const getHighLights = async (req, res) => {
 };
 
 const getFeaturedProducts = async (req, res) => {
-  const categories = [
-    "mens-shopping",
-    "womens-fashion",
-    "watch",
-    "jewellery",
-    "ladies-bag",
-    "accessories",
-    "shoes",
-  ];
-  const featuredProducts = [];
-  for (const category of categories) {
-    const product = await db
-      .collection("products")
-      .findOne({ "category.label": category });
-    if (product) {
-      featuredProducts.push(product);
+  try {
+    const categories = [
+      "mens-shopping",
+      "womens-fashion",
+      "watch",
+      "jewellery",
+      "ladies-bag",
+      "accessories",
+      "shoes",
+    ];
+    const featuredProducts = [];
+    for (const category of categories) {
+      const product = await db
+        .collection("products")
+        .findOne({ "category.label": category });
+      if (product) {
+        featuredProducts.push(product);
+      }
     }
+    res.json(featuredProducts);
+  } catch (error) {
+    console.error("Error fetching featured products:", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
-  res.json(featuredProducts);
 };
 
 module.exports = { getProducts, getFeaturedProducts, getHighLights };
